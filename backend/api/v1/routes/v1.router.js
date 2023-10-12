09fc323b4636a737baeff8652c643039188ba927
rashadar-99 <rashadar99@gmail.com>
2023-10-12 14:25:27 +0530

    feat: add v1 router

--- backend/api/v1/routes/v1.router.js
+++ backend/api/v1/routes/v1.router.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
+const Admin_router = require("./Admin/Admin.router");
+router.use("/Admin", Admin_router);
+
 const User_router = require("./User/User.router");
 router.use("/User", User_router);
 
