29fe978e90c8f784d3a94ce4f95f0488f49f0b97
rashadar-99 <rashadar99@gmail.com>
2023-10-12 14:25:59 +0530

    feat: add jwt_helper

--- backend/api/v1/jwt/jwt_helper.js
+++ backend/api/v1/jwt/jwt_helper.js
@@ -1,24 +1,25 @@
 const createError = require("http-errors");
-const JWT = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-	signAccessToken: (UUID) => {
+	signAccessToken: (ID) => {
 		return new Promise((resolve, reject) => {
 			const payload = {};
 			const secret = process.env.ACCESS_TOKEN_SECRET;
 			const options = {
 				expiresIn: "10m",
 				issuer: process.env.BASE_URL,
-				audience: UUID,
+				audience: ID,
 			};
 
-			JWT.sign(payload, secret, options, (err, token) => {
+			jwt.sign(payload, secret, options, (err, token) => {
 				if (err) {
 					console.error(err.message);
 					reject(createError.InternalServerError());
 				}
 
 				const expiry = new Date(new Date().getTime() + 10 * 60 * 1000).toISOString();
+
 				resolve({ token, expiry });
 			});
 		});
@@ -29,13 +30,45 @@ module.exports = {
 		const authHeader = req.headers["authorization"];
 		const bearerToken = authHeader.split(" ");
 		const token = bearerToken[1];
-		JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
 			if (err) {
 				const message = err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
 				return next(createError.Unauthorized(message));
 			}
 			req.payload = payload;
 			next();
+		});
+	},
+	signRefreshToken: (ID) => {
+		return new Promise((resolve, reject) => {
+			const payload = {};
+			const secret = process.env.REFRESH_TOKEN_SECRET;
+			const options = {
+				expiresIn: "1y",
+				issuer: process.env.BASE_URL,
+				audience: ID,
+			};
+			jwt.sign(payload, secret, options, (err, token) => {
+				if (err) {
+					console.error(err.message);
+					reject(createError.InternalServerError());
+				}
+
+				const expiry = new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString();
+				resolve({ token, expiry });
+			});
+		});
+	},
+	verifyRefreshToken: (refreshToken) => {
+		return new Promise((resolve, reject) => {
+			jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async (err, payload) => {
+				if (err) return reject(createError.Unauthorized());
+
+				const UUID = payload.aud;
+
+				resolve(UUID);
+			});
 		});
 	},
 };
