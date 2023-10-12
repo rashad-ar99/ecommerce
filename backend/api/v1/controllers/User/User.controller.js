8b9f47633ad379fe150686a7afdc19bdc6ecf0bf
rashadar-99 <rashadar99@gmail.com>
2023-10-12 14:23:39 +0530

    feat: add User router and controller

--- backend/api/v1/controllers/User/User.controller.js
+++ backend/api/v1/controllers/User/User.controller.js
@@ -1,6 +1,5 @@
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
-// const User_validation = require("../../validations/User/User.validation");
 
 module.exports = {
 	getUser: async (req, res, next) => {
@@ -9,21 +8,9 @@ module.exports = {
 				status: "success",
 				data: {
 					User: {
-						UUID: res.locals.User.UUID,
-						FirstName: res.locals.User.FirstName,
-						LastName: res.locals.User.LastName,
+						id: res.locals.User.id,
 						Email: res.locals.User.Email,
-						EmailVerified: res.locals.User.EmailVerified,
-						Phone: res.locals.User.Phone,
-						PhoneVerified: res.locals.User.PhoneVerified,
-						PasswordUpdatedAt: res.locals.User.PasswordUpdatedAt,
-						MFAEnabled: res.locals.User.MFAEnabled,
-						MFAMethod: res.locals.User.MFAMethod,
-						PIN: res.locals.User.PIN,
-						Address: res.locals.User.Address,
-						ImageURL: res.locals.User.ImageURL,
-						CreatedAt: res.locals.User.CreatedAt,
-						UpdatedAt: res.locals.User.UpdatedAt,
+						Admin: res.locals.User.Admin,
 					},
 				},
 			});
--- backend/api/v1/routes/User/User.router.js
+++ backend/api/v1/routes/User/User.router.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
 const { verifyAccessToken } = require("../../jwt/jwt_helper");
-// const { verifyLogin } = require("../../middlewares/User/auth.middleware");
-const User_controller = require("../../controllers/User/User.controller");
+const { verifyLogin } = require("../../middlewares/User/auth.middleware");
+const User_controller = require("../../controllers/User/User.controller.js");
 
-// const auth_router = require("./auth.router");
-// router.use("/auth", auth_router);
+router.get("/", verifyAccessToken, verifyLogin, User_controller.getUser);
+
+const auth_router = require("./auth.router");
+router.use("/auth", auth_router);
+
+const Cart_router = require("./Cart/Cart.router");
+router.use("/Cart", Cart_router);
+
+const Coupon_router = require("./Coupon/Coupon.router");
+router.use("/Coupon", Coupon_router);
+
+const Order_router = require("./Order/Order.router");
+router.use("/Order", Order_router);
+
+const Product_router = require("./Product/Product.router");
+router.use("/Product", Product_router);
 
 module.exports = router;
